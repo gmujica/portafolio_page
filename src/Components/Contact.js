e5819b6612601abbeb4000f660ffa7b156e056f3
gmujica <gregorymujica1993@gmail.com>
2020-09-13 00:12:30 -0500

    contact change

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -6,27 +6,27 @@ const Contact = () => {
       <section id="contact">
          <div className='content'>
             <div>
+            <i class="fab fa-linkedin-in"></i>
                <h2>Linkedin</h2>
                <h3>Gregory Mujica</h3>
             </div>
             <div>
+               <i className="fab fa-github"></i>
                <h2>Github</h2>
                <h3>gmujica</h3>
             </div>
             <div>
-               <h2>Discord</h2>
-               <img
-                    //className="header_logo"
-                    src="https://toppng.com/uploads/preview/discord-logo-01-discord-logo-11562849833clsolz2mbc.png"
-                    alt="img"
-                />
+               <i className="fab fa-discord"></i>
+               <h2>Discord</h2> 
                <h3>gmujica#0592</h3>
             </div>
             <div>
+               <i className="far fa-envelope"></i>
                <h2>Gmail</h2>
                <h3>gregorymujica1993@gmail.com</h3>
             </div>
             <div>
+            <i class="fab fa-skype"></i>
                <h2>Skype & Teams</h2>
                <h3>gmujica93@outlook.com</h3>
             </div>
